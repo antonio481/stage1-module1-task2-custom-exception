e26b0e7634a1ab4cf074cec67f618b54846c5985
antonio481 <adanilovx@gmail.com>
2023-10-23 11:51:54 +0400

    Decision commit

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,11 +5,14 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
+  public Student find(long studentID) throws IllegalArgumentException {
+    if (Student.getValueOf(studentID)!=null){
+      return Student.getValueOf(studentID);
+  } else {
+    throw new IllegalArgumentException(
+            "Could not find student with ID " + studentID);
+  }}
+  public static void main(String[] args) throws StudentNotFoundException {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
